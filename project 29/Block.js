fd936606e68f723eb77552ef951784d3ec818e47
lozaBdaniel <72103509+lozaBdaniel@users.noreply.github.com>
2020-11-22 12:48:05 -0600

    Add files via upload

---
+++ project 29/Block.js
@@ -0,0 +1,39 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+
+      score(){
+        if(this.visibility<0 && this.visibility >-105) {
+          score++;
+        }
+        blocks1.score();
+        blocks2.score();
+        blocks3.score();
+        blocks4.score();
+        blocks5.score();
+        blocks6.score();
+        blocks7.score();
+        blocks8.score();
+      }
+}
